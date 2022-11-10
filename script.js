e6f8a46bedc1ceefb35388db529c9f2ef4ee1574
Pedro Henrique Souza Santos <pedrohss466@gmail.com>
2022-11-10 09:50:54 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const toDoField = document.querySelector('.to-do-field');
+const toDoSend = document.querySelector('.to-do-send');
+const toDoList = document.querySelector('.to-do-list')
+
+toDoSend.addEventListener('click' , function(e){
+    const li = document.createElement('li');
+    const buttonClear = document.createElement('button');
+    buttonClear.setAttribute('class' , 'button-clear');
+    buttonClear.innerText = 'Deletar';
+    li.innerText = toDoField.value;
+    li.appendChild(buttonClear);
+    toDoList.appendChild(li);
+}) 
+
+addEventListener('click' , function(e){
+    const el = e.target;
+
+    if(el.classList.contains('button-clear')){
+        el.parentElement.remove();
+    }
+})
