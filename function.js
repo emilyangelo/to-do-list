280e4dac3a92c6fdec204e56b605e24f2660051e
emilyangelo <emily09.e.y@gmail.com>
2022-11-10 11:02:42 -0300

    primeiro commit

---
+++ function.js
@@ -0,0 +1,17 @@
+const x = 5;
+const y = 7;
+const z = 9;
+
+function digaola(){
+    console.log('ola');
+}
+
+function multiplica(a, b, t){
+    return a*b*t;
+    
+}
+
+digaola();
+const resultado = multiplica(x, y, z);
+
+console.log(resultado);
--- script.js
+++ script.js
@@ -2,15 +2,37 @@ const toDoField = document.querySelector('.to-do-field');
 const toDoSend = document.querySelector('.to-do-send');
 const toDoList = document.querySelector('.to-do-list')
 
-toDoSend.addEventListener('click' , function(e){
-    const li = document.createElement('li');
+function criali(){
+  const li = document.createElement('li');
+  return li;
+}
+
+function criaButtonClear(){
     const buttonClear = document.createElement('button');
     buttonClear.setAttribute('class' , 'button-clear');
     buttonClear.innerText = 'Deletar';
-    li.innerText = toDoField.value;
-    li.appendChild(buttonClear);
-    toDoList.appendChild(li);
-}) 
+    return buttonClear;
+}
+
+        function criatarefa(li, buttonClear){
+        li.innerText = toDoField.value;
+        li.appendChild(buttonClear);
+        toDoList.appendChild(li);
+        }
+
+toDoSend.addEventListener('click' , function(e){
+    const li = criali();
+    const buttonClear = criaButtonClear();
+    criatarefa(li, buttonClear);   
+});
+
+toDoField.addEventListener('keyup', function(e){
+    if(e.ketCode === 13){
+        const li = criali();
+        const buttonClear = criaButtonClear();
+        criatarefa(li, buttonClear);
+ }
+})
 
 addEventListener('click' , function(e){
     const el = e.target;
